de03ab746e6bed0c40d52cdbb815dd792a47c2f8
wley3337 <wley3337@gmail.com>
2018-06-26 15:37:16 -0400

    pirates ahoy

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,19 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+
+
+  @@all =[]
+
+  def self.all
+    @@all
+  end
+
+  def initialize(pirate)
+    @name = pirate[:name]
+    @weight = pirate[:weight]
+    @height = pirate[:height]
+    @@all << self
+  end
+
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+  attr_accessor :name, :type, :booty
+
+  @@all =[]
+
+  def self.all
+    @@all
+  end
+
+  def initialize(ship)
+    @name = ship[:name]
+    @type = ship[:type]
+    @booty = ship[:booty]
+    @@all<< self
+  end
+
+  def self.clear
+    self.all.clear
+  end
 end
